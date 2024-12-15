eed02366cec53378a254f28f5895abec89c40f63
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2024-12-15 18:53:19 -0500

    adding quick sort code

---
+++ BasicPrograms/src/main/java/com/sort/QuickSort.java
@@ -0,0 +1,40 @@
+package com.sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    static int partition(int[] arr, int p, int r) {
+        int x = arr[r];
+        int i = p - 1;
+        for (int j = p; j <= r - 1; j++) {
+            if (arr[j] < x) {
+                i = i + 1;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, r);
+        return i + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void quickSort(int[] arr, int p, int r) {
+        if (p < r) {
+            int q = partition(arr, p, r);
+            quickSort(arr, p, q - 1);
+            quickSort(arr, q + 1, r);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
