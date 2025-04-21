d9188182064d65f55af08e61e9175742fbd04fcc
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-04-20 22:11:09 -0400

    updating solution for NeetCode17

--- BasicPrograms/src/main/java/com/neetcode/NeetCode17.java
+++ BasicPrograms/src/main/java/com/neetcode/NeetCode17.java
@@ -3,28 +3,22 @@ package com.neetcode;
 public class NeetCode17 {
 
     static int characterReplacement(String s, int k) {
-        int left = 0, maxFreq = 0, winSize = 0, maxWin = 0;
+        int maxFreq = 0, maxWin = 0;
         int[] freq = new int[26];
 
-        for (int right = 0; right < s.length(); right++) {
-            freq[s.charAt(right) - 'A']++;
-
-            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
-
-            winSize = right - left + 1;
-            if (winSize - maxFreq > k) {
-                freq[s.charAt(right) - 'A']--;
-                left++;
+        for (int left = 0, right = 0; right < s.length(); right++) {
+            maxFreq = Math.max(maxFreq, ++freq[s.charAt(right) - 'A']);
+            while (right - left + 1 - maxFreq > k) {
+                freq[s.charAt(left++) - 'A']--;
             }
-            winSize = right - left + 1;
-            maxWin = Math.max(maxWin, winSize);
+            maxWin = Math.max(maxWin, right - left + 1);
         }
         return maxWin;
     }
 
     public static void main(String[] args) {
-        String s = "XYYX";
-        int k = 2;
+        String s = "AABABBA";
+        int k = 1;
         System.out.println(characterReplacement(s, k));
     }
 }
