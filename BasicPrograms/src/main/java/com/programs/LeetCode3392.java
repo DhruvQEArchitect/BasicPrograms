20d6414653623f76306b34252e9b2a9ac359b05c
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-04-27 17:30:26 -0400

    adding solution for Leetcode 3392

---
+++ BasicPrograms/src/main/java/com/programs/LeetCode3392.java
@@ -0,0 +1,21 @@
+package com.programs;
+
+public class LeetCode3392 {
+
+    static int countSubarrays(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+                count++;
+
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, -4, -1, 4};
+        System.out.println(countSubarrays(arr));
+    }
+}
