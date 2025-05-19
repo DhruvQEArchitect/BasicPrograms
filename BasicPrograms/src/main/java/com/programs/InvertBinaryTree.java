f9a9ed64fb8ccc07780fbe88a2726599bc6b085c
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-05-19 19:39:47 -0400

    adding solution for LeetCode226

---
+++ BasicPrograms/src/main/java/com/programs/InvertBinaryTree.java
@@ -0,0 +1,13 @@
+package com.programs;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode leftNode = invertTree(root.left);
+        TreeNode rightNode = invertTree(root.right);
+        root.left = rightNode;
+        root.right = leftNode;
+        return root;
+    }
+}
