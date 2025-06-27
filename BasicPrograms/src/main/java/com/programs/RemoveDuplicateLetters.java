16c16f018d621c334e13bcfb0efdce6228f3fe13
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-06-26 22:56:07 -0400

    adding solution for LeetCode2235

---
+++ BasicPrograms/src/main/java/com/programs/RemoveDuplicateLetters.java
@@ -0,0 +1,40 @@
+package com.programs;
+
+import java.util.*;
+
+public class RemoveDuplicateLetters {
+
+    static String removeDuplicateLetters(String s) {
+        boolean[] seen = new boolean[26];
+        int[] lastIndex = new int[26];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int index = ch - 'a';
+
+            if (seen[index]) continue;
+
+            while (!stack.isEmpty() && ch < stack.peek() && i < lastIndex[stack.peek() - 'a']) {
+                seen[stack.pop() - 'a'] = false;
+            }
+            stack.push(ch);
+            seen[index] = true;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (char ch : stack)
+            stringBuilder.append(ch);
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        String string = "cbacdcbc";
+        System.out.println(removeDuplicateLetters(string));
+
+    }
+}
