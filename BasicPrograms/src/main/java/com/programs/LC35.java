01c09d63d01f0ce583d03acdba36750ace072ff1
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-07-28 20:40:01 -0400

    adding solution for LeetCode35

---
+++ BasicPrograms/src/main/java/com/programs/LC35.java
@@ -0,0 +1,21 @@
+package com.programs;
+
+public class LC35 {
+
+    static int searchInsert(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6};
+        int target = 2;
+        System.out.println(searchInsert(arr, target));
+    }
+}
