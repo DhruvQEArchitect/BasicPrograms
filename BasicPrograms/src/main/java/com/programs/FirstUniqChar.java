e8d27970f57158fe4379661d0121642f196910e7
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-08-02 21:51:03 -0400

    updating solution for LeetCode387

--- BasicPrograms/src/main/java/com/programs/FirstUniqChar.java
+++ BasicPrograms/src/main/java/com/programs/FirstUniqChar.java
@@ -5,16 +5,25 @@ import java.util.*;
 public class FirstUniqChar {
 
     static int firstUniqChar(String s) {
-        HashMap<Character, Integer> hashMap = new LinkedHashMap<>();
+//        HashMap<Character, Integer> hashMap = new LinkedHashMap<>();
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            hashMap.put(s.charAt((i)), hashMap.getOrDefault(s.charAt(i), 0) + 1);
+//        }
+//
+//        for (Character value : hashMap.keySet()) {
+//            if (hashMap.get(value).equals(1)) {
+//                return s.indexOf(value);
+//            }
+//        }
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
 
         for (int i = 0; i < s.length(); i++) {
-            hashMap.put(s.charAt((i)), hashMap.getOrDefault(s.charAt(i), 0) + 1);
-        }
-
-        for (Character value : hashMap.keySet()) {
-            if (hashMap.get(value).equals(1)) {
-                return s.indexOf(value);
-            }
+            if (freq[s.charAt(i) - 'a'] == 1)
+                return i;
         }
         return -1;
     }
