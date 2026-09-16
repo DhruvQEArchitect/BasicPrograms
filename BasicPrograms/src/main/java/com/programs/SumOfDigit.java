71de7d251f1df66798ca768a1df10264cdf5496c
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2026-09-16 12:54:48 +0530

    adding another approach

--- BasicPrograms/src/main/java/com/programs/SumOfDigit.java
+++ BasicPrograms/src/main/java/com/programs/SumOfDigit.java
@@ -10,6 +10,16 @@ public class SumOfDigit {
 
     public static void main(String[] args) {
         System.out.println(sumOfDigit(12345));
+        System.out.println(getSum(1234));
 
+    }
+
+    static int getSum(int num) {
+        int sum = 0;
+        while (num % 10 != 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        return sum;
     }
 }
